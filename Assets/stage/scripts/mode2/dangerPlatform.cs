5c2bc43733826f6541312ca7cd50409f86db03ee
baconNoir <uadsgdiasdf@gmail.com>
2023-02-08 10:35:05 +0800

    修bug前 先準備好檔案

--- Assets/stage/scripts/mode2/dangerPlatform.cs
+++ Assets/stage/scripts/mode2/dangerPlatform.cs
@@ -6,11 +6,26 @@ public class dangerPlatform : MonoBehaviour
 {
     public bool active;
 
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     public void dangerOn()
     {
         if (!active) 
         {
             active = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "DeadZone")
+        {
+
         }
     }
 
